bcd73bea596ae69850797123f2f5fe67e9584a94
JoshuaC930 <joshuacorrales930@u.boisestate.edu>
2023-10-31 09:57:30 -0600

    pogress

--- App.js
+++ App.js
@@ -2,19 +2,47 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Camera } from 'expo-camera';
 // import PreviewList from './PreviewList';
 // import ImageGrid from './ImageGrid'
-import { Image, Text, View, TouchableOpacity, StatusBar, Animated, VirtualizedList, Alert, useWindowDimensions, Component, Button } from 'react-native';
+import { Image, Text, View, TouchableOpacity, StatusBar, Animated, VirtualizedList, Alert, useWindowDimensions, Component, Button, Modal } from 'react-native';
 //import Modal from 'react-native-modal'
 
 import { styles } from './src/styles';
+
+const renderItem = ({ item }) => {
+  var backgroundColor, color;
+
+  backgroundColor = item.selected ? '#349eeb' : 'white';
+  color = item.selected ? 'white' : 'black';
+
+  return (<Item
+      item={item}
+      onPress={() => { toggleList(item.key) }}
+      backgroundColor={{ backgroundColor }}
+      textColor={{ color }}
+  />);
+
+  // called when a list item is touched
+  function toggleList(key) {
+      const newList = list.map((item) => {
+          if (item.key == key) item.selected = item.selected ? false : true;
+          return item;
+      });
+
+      setList([...newList]);
+  }
+}
 
 const CameraApp = () => {
     //instance variables
     const camRef = useRef(Camera);
     const emptyData = [];
+    const getItemCount = (data) => list.length;
+    const getItem = (data, index) => (list[index]);
 
     //state variables
     const [hasCameraPermission, setCameraPermission] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
+    const [visible, setVisible] = useState(false);
+    const [list, setList] = useState(emptyData);
 
     //run after each redraw
     useEffect(() => {
@@ -26,22 +54,66 @@ const CameraApp = () => {
       }
   });
 
-  //function for flipping camera
-  const flipCamera = () => setType(
-    type === Camera.Constants.Type.back ?
-        Camera.Constants.Type.front : Camera.Constants.Type.back
-  )
-
   // if do not have permission to access camera show notice
   if (hasCameraPermission === null || hasCameraPermission === false) {
     var notice = <View><Text>No Access To Cameara</Text></View>;
     return notice;
   }
 
+  //function for flipping camera
+  const flipCamera = () => setType(
+    type === Camera.Constants.Type.back ?
+        Camera.Constants.Type.front : Camera.Constants.Type.back
+  )
+
+  //modal object for gallary and other functions.
+  const GalleryModal = ({visible}) => {
+    return (
+      <Modal visible={visible} transparent={true} style={{alignItems: 'center'}}>
+        <View style={styles.modal}>
+          <Text style={styles.title}>Gallery</Text>
+            <View>
+              <VirtualizedList
+                style={styles.list}
+                data={emptyData}
+                initialNumToRender={4}
+                renderItem={renderItem}
+                keyExtractor={(item,index) => index}
+                getItemCount={getItemCount}
+                getItem={getItem}
+              />
+            </View>
+            <View style={styles.buttonLayout}>
+              <Button title='Share' style={styles.button} textStyle={styles.buttonText} onPress={() => console.log('Share pressed!')}/>
+              <Button title='Save' style={styles.button} textStyle={styles.buttonText} onPress={() => console.log('Save pressed!')}/>
+              <Button title='Delete' style={styles.button} textStyle={styles.buttonText} onPress={() => console.log('Delete pressed!')}/>
+              <Button title='Close' style={styles.button} textStyle={styles.buttonText} onPress={() => setVisible(!visible)}/>
+            </View>
+        </View>
+      </Modal>
+    );
+  }
+
+  //function for taking picture and adding it to list
+  const snap = async () => {
+    
+    if (camRef.current) {
+      const options = { quality: 1, base64: true }
+      let photo = await camRef.current.takePictureAsync(options);
+      const newList = [photo.uri].concat(list)
+      photo.name = 'IMG' + list.length;
+      setList(newList);
+      console.log('Snap Pressed! Photo Added');
+    };
+  }
+
+  //TODO: Write function for sharing pictures
+  //TODO: Write function for saving pictures
+  //TODO: Write fuction for deleting pictures
+
     var cameraUi =
     <>
       <View style={styles.container}>
-          
           <View style={styles.cameraContainer}>
               <Camera
                   ref={ref => {camRef.current = ref; }}
@@ -53,14 +125,16 @@ const CameraApp = () => {
 
       <View style={styles.actionRow}>
         <View style={styles.buttonLayout}>
-          <Button title='Flip' onPress={flipCamera}/>
-          <Button title='Snap' onPress={() => console.log("Snap Pressed!")}/>
-          <Button title='Gallery' onPress={() => console.log("Gallery Pressed!")}/>
+          <TouchableOpacity onPress={flipCamera}><Text>Flip</Text></TouchableOpacity>
+          <TouchableOpacity onPress={snap}><Text>Snap</Text></TouchableOpacity>
+          <TouchableOpacity onPress={() => setVisible(!visible)}><Text>Gallery</Text></TouchableOpacity>
         </View>
+
+        <GalleryModal
+          visible={visible}
+        />
       </View>
     </>
-        
-
     return (cameraUi);
 }
 
--- src/styles.js
+++ src/styles.js
@@ -15,6 +15,7 @@ export const styles = StyleSheet.create ({
         aspectRatio: 3 / 4,
     },
     actionRow: {
+        flex: 1,
         backgroundColor: 'rgba(255, 255, 255, 0.65)',
         height: Dimensions.get('screen').height * 0.10,
         width: Dimensions.get('screen').width,
@@ -24,8 +25,30 @@ export const styles = StyleSheet.create ({
         bottom: 0,
     },
     buttonLayout: {
-        paddingTop: 24,
+        paddingTop: 35,
         flexDirection: 'row',
         justifyContent: 'space-evenly',
+    },
+    modal: {
+        backgroundColor: 'rgba(255, 255, 255, 1)',
+        borderRadius: 25,
+        position: 'absolute', 
+        height: Dimensions.get('screen').height * 0.80,
+        width: Dimensions.get('screen').width * 0.97,
+    },
+    title: {
+        paddingTop: 20,
+        fontSize: 36,
+        fontWeight: 'bold',
+        paddingLeft: 20
+    },
+    list: {
+        marginTop: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 10,
+        borderWidth: 2,
+        borderColor: 'gray',
+        height: 500
     },
 });
