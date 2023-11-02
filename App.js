6ec57d8b80e5a30840592233e0bf04f1ea4082e0
JoshuaC930 <joshuacorrales930@u.boisestate.edu>
2023-11-01 22:14:27 -0600

    mostly done

--- App.js
+++ App.js
@@ -1,35 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Camera } from 'expo-camera';
-// import PreviewList from './PreviewList';
-// import ImageGrid from './ImageGrid'
-import { Image, Text, View, TouchableOpacity, StatusBar, Animated, VirtualizedList, Alert, useWindowDimensions, Component, Button, Modal } from 'react-native';
-//import Modal from 'react-native-modal'
+import * as Sharing from 'expo-sharing';
+import * as MediaLibrary from 'expo-media-library';
+import { Image, Text, View, TouchableOpacity, VirtualizedList, Button, Modal, Dimensions, Alert } from 'react-native';
+import Toast from 'react-native-root-toast'
 
 import { styles } from './src/styles';
 
-const renderItem = ({ item }) => {
-  var backgroundColor, color;
-
-  backgroundColor = item.selected ? '#349eeb' : 'white';
-  color = item.selected ? 'white' : 'black';
-
-  return (<Item
-      item={item}
-      onPress={() => { toggleList(item.key) }}
-      backgroundColor={{ backgroundColor }}
-      textColor={{ color }}
-  />);
-
-  // called when a list item is touched
-  function toggleList(key) {
-      const newList = list.map((item) => {
-          if (item.key == key) item.selected = item.selected ? false : true;
-          return item;
-      });
-
-      setList([...newList]);
-  }
-}
 
 const CameraApp = () => {
     //instance variables
@@ -37,104 +14,216 @@ const CameraApp = () => {
     const emptyData = [];
     const getItemCount = (data) => list.length;
     const getItem = (data, index) => (list[index]);
+    const toast = {
+        backgroundColor: 'white',
+        textColor: 'black',
+        duration: Toast.durations.LONG,
+        opacity: 1,
+        position: Toast.positions.CENTER,
+    }
 
     //state variables
     const [hasCameraPermission, setCameraPermission] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
-    const [visible, setVisible] = useState(false);
+    const [galleryVisible, setGalleryVisible] = useState(false);
+    const [viewerVisible, setViewerVisible] = useState(false);
+    const [viewPhoto, setViewPhoto] = useState(null);
     const [list, setList] = useState(emptyData);
+    const [imgIndex, setImgIndex] = useState(-1);
+    const [mediaPermissions, setMediaPermissions] = MediaLibrary.usePermissions();
+
+    //components
+    const Item = ({ item, onPress }) => (
+        <TouchableOpacity style={[styles.item]} onPress={onPress}>
+            <View style={styles.itemContainer}>
+                <Image style={styles.imageContainer} source={{ uri: item.uri }} />
+                <Text style={[styles.itemText]}>{item.name}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+
+    //modal object for gallary and other functions.
+    const GalleryModal = ({ visible }) => (
+        <Modal animationType={'slide'} visible={visible} transparent={true} style={{ alignItems: 'center' }}>
+            <View style={styles.centered}>
+                <View style={styles.galleryModal}>
+                    <Text style={styles.title}>Gallery</Text>
+                    <Text style={styles.instructions}>Tap on a photo to view it in the Image Viewer</Text>
+                    <View style={{borderRadius: 25, overflow: 'hidden'}}>
+                        <VirtualizedList
+                            style={styles.list}
+                            data={emptyData}
+                            initialNumToRender={4}
+                            renderItem={renderItem}
+                            keyExtractor={(item, index) => index}
+                            getItemCount={getItemCount}
+                            getItem={getItem}
+                        />
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'flex-end' }}>
+                        <Button title='Close' style={styles.button} textStyle={styles.buttonText} onPress={() => { setGalleryVisible(!visible) }} />
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+
+    const renderItem = ({ item, index }) => {
+        return (<Item
+            item={item}
+            onPress={() => { setImgIndex(index), showImage(item) }}
+        />);
+    }
+
+    //function for viewing selected photo
+    const showImage = (selectedImage) => {
+        setGalleryVisible(false);
+        setViewPhoto(selectedImage);
+        setViewerVisible(true);
+    }
+
+    //modal object for viewing selected image
+    const ViewerModal = ({ visible, image }) => (
+        <Modal animationType={'slide'} visible={visible} transparent={true} style={{ alignItems: 'center' }}>
+            <View style={styles.centered}>
+                <View style={styles.viewerModal}>
+                    <Text style={styles.title}>Image Preview</Text>
+                    <Text style={styles.instructions}>Use the controls below</Text>
+                    <Image source={{ uri: image == null ? null : image.uri }} style={styles.viewerImage} />
+                    <Text style={{ fontSize: 16, textAlign: 'center' }}>{image == null ? "" : image.name}</Text>
+                    <View style={styles.buttonLayout}>
+                        <Button title='Share' style={styles.button} textStyle={styles.buttonText} onPress={shareImage} />
+                        <Button title='Save' style={styles.button} textStyle={styles.buttonText} onPress={saveImage} />
+                        <Button title='Delete' style={styles.button} textStyle={styles.buttonText} onPress={deleteImage} />
+                        <Button title='Close' style={styles.button} textStyle={styles.buttonText} onPress={() => setViewerVisible(!visible)} />
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
 
     //run after each redraw
     useEffect(() => {
-      if (hasCameraPermission === null) {
-          (async () => {
-              const { status } = await Camera.requestCameraPermissionsAsync();
-              setCameraPermission(status === 'granted');
-          })();
-      }
-  });
+        if (hasCameraPermission === null) {
+            (async () => {
+                const { status } = await Camera.requestCameraPermissionsAsync();
+                setCameraPermission(status === 'granted');
+            })();
+        }
+    });
 
-  // if do not have permission to access camera show notice
-  if (hasCameraPermission === null || hasCameraPermission === false) {
-    var notice = <View><Text>No Access To Cameara</Text></View>;
-    return notice;
-  }
+    // if do not have permission to access camera show notice
+    if (hasCameraPermission === null || hasCameraPermission === false) {
+        var notice = <View style={{ justifyContent: 'center', fontSize: 36, }}><Text>No Access To Cameara</Text></View>;
+        return notice;
+    }
 
-  //function for flipping camera
-  const flipCamera = () => setType(
-    type === Camera.Constants.Type.back ?
-        Camera.Constants.Type.front : Camera.Constants.Type.back
-  )
+    //function for flipping camera
+    const flipCamera = () => setType(
+        type === Camera.Constants.Type.back ?
+            Camera.Constants.Type.front : Camera.Constants.Type.back
+    )
 
-  //modal object for gallary and other functions.
-  const GalleryModal = ({visible}) => {
-    return (
-      <Modal visible={visible} transparent={true} style={{alignItems: 'center'}}>
-        <View style={styles.modal}>
-          <Text style={styles.title}>Gallery</Text>
-            <View>
-              <VirtualizedList
-                style={styles.list}
-                data={emptyData}
-                initialNumToRender={4}
-                renderItem={renderItem}
-                keyExtractor={(item,index) => index}
-                getItemCount={getItemCount}
-                getItem={getItem}
-              />
-            </View>
-            <View style={styles.buttonLayout}>
-              <Button title='Share' style={styles.button} textStyle={styles.buttonText} onPress={() => console.log('Share pressed!')}/>
-              <Button title='Save' style={styles.button} textStyle={styles.buttonText} onPress={() => console.log('Save pressed!')}/>
-              <Button title='Delete' style={styles.button} textStyle={styles.buttonText} onPress={() => console.log('Delete pressed!')}/>
-              <Button title='Close' style={styles.button} textStyle={styles.buttonText} onPress={() => setVisible(!visible)}/>
-            </View>
-        </View>
-      </Modal>
-    );
-  }
+    //function for taking picture and adding it to list
+    const snap = async () => {
+        if (camRef.current) {
+            const date = new Date();
+            const dateString = date.toLocaleDateString('en-IT', {hour12: false}).replaceAll('/', '');
+            const timeString =  date.toLocaleTimeString('en-IT', {hour12: false}).replaceAll(':', '');
 
-  //function for taking picture and adding it to list
-  const snap = async () => {
-    
-    if (camRef.current) {
-      const options = { quality: 1, base64: true }
-      let photo = await camRef.current.takePictureAsync(options);
-      const newList = [photo.uri].concat(list)
-      photo.name = 'IMG' + list.length;
-      setList(newList);
-      console.log('Snap Pressed! Photo Added');
-    };
-  }
+            var imgTimeStamp = dateString + '-' + timeString + "." + date.getMilliseconds();
 
-  //TODO: Write function for sharing pictures
-  //TODO: Write function for saving pictures
-  //TODO: Write fuction for deleting pictures
+            const options = { quality: 1, base64: false, height: Dimensions.get('screen').height, width: Dimensions.get('screen').width }
+            let photo = await camRef.current.takePictureAsync(options);
+            photo.name = 'IMG_' + imgTimeStamp;
+            const newList = [photo].concat(list)
+
+            setList(newList);
+            console.log('Image Taken: ' + JSON.stringify(newList.at(0).name))
+        };
+    }
+
+    // function for sharing selected image
+    const shareImage = async () => {
+        try {
+            await Sharing.shareAsync(list[imgIndex].uri);
+            setViewerVisible(!viewerVisible)
+            Toast.show(list[imgIndex].name + ' shared successfully', toast);
+        }
+        catch (error) {
+            console.error('Error sharing image: ' + error)
+        }
+    }
+
+    // function for saving selected image to device
+    const saveImage = async () => {
+        if (!mediaPermissions.granted)
+            setMediaPermissions();
+        else {
+            try {
+                setViewerVisible(!viewerVisible)
+                await MediaLibrary.saveToLibraryAsync(list[imgIndex].uri);
+                Toast.show(list[imgIndex].name + ' saved to device', toast);
+            }
+            catch (error) {
+                console.error('Error saving image: ' + error);
+            }
+        }
+    }
+
+    // function for deleting selected image
+    const deleteImage = () => {
+        Alert.alert('Are you sure you want to delete this photo?', 'This action is irreversable.', [
+            {
+                text: 'Yes',
+                onPress: () => {
+                    setViewerVisible(!viewerVisible)
+                    
+                    const newList = [];
+                    list.map((image, index) => {
+                        if (imgIndex != index) 
+                            newList.push(image)
+                        else 
+                        Toast.show(image.name + ' deleted', toast);
+                    })
+                    setList([...newList]);
+                },
+            },
+            {
+                text: 'No',
+            }
+        ])
+    }
 
     var cameraUi =
-    <>
-      <View style={styles.container}>
-          <View style={styles.cameraContainer}>
-              <Camera
-                  ref={ref => {camRef.current = ref; }}
-                  style={styles.camera}
-                  type={type}
-              />
-          </View>
-      </View>
+        <>
+            <View style={styles.container}>
+                <View style={styles.cameraContainer}>
+                    <Camera
+                        ref={ref => { camRef.current = ref; }}
+                        style={styles.camera}
+                        type={type}
+                    />
+                </View>
+            </View>
+            <View style={styles.actionRow}>
+                <View style={styles.buttonLayout}>
+                    <TouchableOpacity onPress={flipCamera}>
+                        <Image style={styles.controlButtons} source={require('./assets/flip.png')}/>
+                    </TouchableOpacity>
 
-      <View style={styles.actionRow}>
-        <View style={styles.buttonLayout}>
-          <TouchableOpacity onPress={flipCamera}><Text>Flip</Text></TouchableOpacity>
-          <TouchableOpacity onPress={snap}><Text>Snap</Text></TouchableOpacity>
-          <TouchableOpacity onPress={() => setVisible(!visible)}><Text>Gallery</Text></TouchableOpacity>
-        </View>
+                    <TouchableOpacity onPress={snap}>
+                        <Image style={styles.controlButtons} source={require('./assets/snap.png')}/>
+                    </TouchableOpacity>
 
-        <GalleryModal
-          visible={visible}
-        />
-      </View>
-    </>
+                    <TouchableOpacity onPress={() => setGalleryVisible(!galleryVisible)}>
+                        <Image style={styles.galleryPreview} source={list.length == 0 ? require('./assets/gallery.png') : list[0]}/>
+                    </TouchableOpacity>
+                </View>
+                <GalleryModal visible={galleryVisible} />
+                <ViewerModal visible={viewerVisible} image={viewPhoto} />
+            </View>
+        </>
     return (cameraUi);
 }
 
--- src/styles.js
+++ src/styles.js
@@ -1,18 +1,21 @@
-import { Dimensions, StyleSheet } from "react-native";
+import { Dimensions, StyleSheet, Camera } from "react-native";
+
+// const aspectRatio = await Camera.getSupportedRatiosAsync();
 
 export const styles = StyleSheet.create ({
     container: {
         flex: 1,
+        backgroundColor: 'black'
     },
     cameraContainer: {
-        flexGrow: 1
+        flexGrow: 1,
     },
     camera: {
         marginTop: 'auto',
         marginBottom: 'auto',
-        height: Dimensions.get('screen').height,
-        width: Dimensions.get('screen').width,
-        aspectRatio: 3 / 4,
+        width: Dimensions.get('window'),
+        height: Dimensions.get('window'),
+        aspectRatio:  10 / 16
     },
     actionRow: {
         flex: 1,
@@ -25,30 +28,87 @@ export const styles = StyleSheet.create ({
         bottom: 0,
     },
     buttonLayout: {
-        paddingTop: 35,
+        paddingTop: 20,
         flexDirection: 'row',
         justifyContent: 'space-evenly',
     },
-    modal: {
+    galleryModal: {
         backgroundColor: 'rgba(255, 255, 255, 1)',
         borderRadius: 25,
-        position: 'absolute', 
         height: Dimensions.get('screen').height * 0.80,
         width: Dimensions.get('screen').width * 0.97,
     },
+    viewerModal: { 
+        backgroundColor: 'rgba(255, 255, 255, 1)',
+        borderRadius: 25,
+        height: Dimensions.get('screen').height * 0.90,
+        width: Dimensions.get('screen').width * 0.97, 
+    },
+    viewerImage: {
+        flex: 0.95,
+        alignItems: 'center',
+        margin: 5,
+        borderRadius: 25,
+        borderColor: 'black',
+        borderWidth: 2
+    },
     title: {
-        paddingTop: 20,
         fontSize: 36,
         fontWeight: 'bold',
+        paddingTop: 5,
+        paddingLeft: 20
+    },
+    instructions: {
+        fontSize: 14,
         paddingLeft: 20
     },
     list: {
-        marginTop: 20,
+        marginTop: 5,
         marginLeft: 10,
         marginRight: 10,
         marginBottom: 10,
         borderWidth: 2,
         borderColor: 'gray',
-        height: 500
+        borderRadius: 25,
+        width: Dimensions.get('screen').width * 0.92,
+        height: Dimensions.get('screen').height * 0.64,
     },
+    itemContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    item: {
+        paddingLeft: 5,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderBottomWidth: 1,
+        borderColor: 'gray',
+        
+    },
+    itemText: {
+        fontSize: 16
+    },
+    imageContainer: {
+        width: 100,
+        height: 100,
+        marginRight: 5,
+        overflow: 'hidden',
+        borderRadius: 25,
+    },
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    controlButtons: {
+        width: 48,
+        height: 48,
+    },
+    galleryPreview: {
+        width: 48,
+        height: 48,
+        borderColor: 'black',
+        borderRadius: 10,
+        borderWidth: 3,
+    }
 });
